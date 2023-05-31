13cf61adf4b92c20b85e6340c3e5cbae8f664c30
shubh9752 <61161071+shubh9752@users.noreply.github.com>
2023-05-31 21:02:52 +0530

    css updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import DataGrid from './DataGrid';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Posts</h2>
+      <div className='container'><h2>Posts</h2></div>
       <DataGrid
         apiEndpoint="https://jsonplaceholder.typicode.com/posts"
         columns={['userId', 'id', 'title', 'body']}
       />
-      <h2>Comments</h2>
+      <div className='container'><h2>Comments</h2></div>
       <DataGrid
         apiEndpoint="https://jsonplaceholder.typicode.com/comments"
         columns={['postId', 'id', 'name', 'email', 'body']}
